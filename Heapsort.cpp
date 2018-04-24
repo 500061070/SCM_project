367551129b0d0ec9333364649a44ad209f3f2cad
Your Name <you@example.com>
2018-04-24 12:46:14 +0530

    Program on heap sort

---
+++ Heapsort.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+ 
+// To heapify a subtree rooted with node i which is
+// an index in arr[]. n is size of heap
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;  // Initialize largest as root
+    int l = 2*i + 1;  // left = 2*i + 1
+    int r = 2*i + 2;  // right = 2*i + 2
+ 
+    // If left child is larger than root
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+ 
+    // If right child is larger than largest so far
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+ 
+    // If largest is not root
+    if (largest != i)
+    {
+        swap(arr[i], arr[largest]);
+ 
+        // Recursively heapify the affected sub-tree
+        heapify(arr, n, largest);
+    }
+}
+ 
+// main function to do heap sort
+void heapSort(int arr[], int n)
+{
+    // Build heap (rearrange array)// to be added by pratyush
+   
+ 
+    // One by one extract an element from heap // to be added by rachit
+   
+}
+ 
+/* A utility function to print array of size n */
+void printArray(int arr[], int n) // to be added by saurabh
+{
+  
+}
+ 
+// Driver program
+int main() // to be added by sajal
+{
+   
+}
