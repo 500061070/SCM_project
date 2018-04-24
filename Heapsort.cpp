d1c091dc74e89831abfbe204407dff689a3fd5ba
Your Name <you@example.com>
2018-04-24 14:57:32 +0530

    Commited by Pratyush

--- Heapsort.cpp
+++ Heapsort.cpp
@@ -30,10 +30,21 @@ void heapify(int arr[], int n, int i)
 // main function to do heap sort
 void heapSort(int arr[], int n)
 {
-    // Build heap (rearrange array)// to be added by pratyush
-   
+    
+  // Build heap (rearrange array)
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
  
-    // One by one extract an element from heap // to be added by rachit
+    // One by one extract an element from heap
+    for (int i=n-1; i>=0; i--)
+    {
+        // Move current root to end
+        swap(arr[0], arr[i]);
+ 
+        // call max heapify on the reduced heap
+        heapify(arr, i, 0);
+    } 
+   
    
 }
  
